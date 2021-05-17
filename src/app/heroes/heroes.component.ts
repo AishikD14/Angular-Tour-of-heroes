8406318d4e72dd586506206197324b67cc3dbfad
Aishik Deb <debaishik14@gmail.com>
2021-05-17 20:33:13 +0530

    first commit

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.scss']
+})
+export class HeroesComponent implements OnInit {
+
+  heros: Hero[] = [];
+
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heros => this.heros = heros);
+  }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heros.push(hero);
+      });
+  }
+
+  delete(hero: Hero): void {
+    this.heros = this.heros.filter(h => h !== hero);
+    this.heroService.deleteHero(hero.id).subscribe();
+  }
+}
